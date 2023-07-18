cb7c725740546b72c5d10884ecd81d714f50a8e7
Laba Subagia <labasubagia22@gmail.com>
2023-07-18 15:32:07 +0800

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="m-2" size={'lg'}>
+        Click Me
+      </Button>
+    </div>
+  );
+}
